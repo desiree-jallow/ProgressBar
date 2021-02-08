5ec281ef715fc00793316c07188e1b5cb4a85854
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-02-08 15:26:31 -0600

    Initial Commit

---
+++ ProgressBar/ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarApp.swift
+//  ProgressBar
+//
+//  Created by Desiree on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
