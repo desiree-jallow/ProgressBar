7a0b1a259af1a5d726735d2fa5ac6b49ced11e36
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-02-08 16:28:02 -0600

    create progress bar

--- ProgressBar/ContentView.swift
+++ ProgressBar/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AudioBookCell()
+        
+    }
+}
+
+struct AudioBookCell: View {
+    var body: some View {
+        VStack {
+            Image("sampleOne")
+            ProgressView(progressPercent: 0.3)
+        }
+        .frame(width: 200)
+    }
+}
+struct ProgressView: View {
+    var progressPercent = 1.0
+    var body: some View {
+        ZStack {
+            GeometryReader { geo in
+                Rectangle()
+                    .fill(Color.gray)
+                    .frame(height: 5)
+                Rectangle()
+                .fill(Color.orange)
+                .frame(width: geo.size.width * CGFloat(self.progressPercent), height: 5)
+            }
+        }
     }
 }
 
